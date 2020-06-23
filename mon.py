296c61c32f360c7213fcf246a470f3ab32715c47
Daniel Li <daniel442li@gmail.com>
2020-06-22 23:28:01 -0400

    Main Monitor

--- mon.py
+++ mon.py
@@ -5,9 +5,7 @@ from discord_webhooks import DiscordWebhooks
 import datetime
 from datetime import timedelta 
 
-WEBHOOK_URL = 'https://discordapp.com/api/webhooks/716398429995270174/H3mAVClDg6gEnsjNipNe4hsePPwHx37UYIR9b5PFDFf8-jKrXkP8rOEQ5ipMjSRMmEdB'
-webhook = DiscordWebhooks(WEBHOOK_URL)
-
+webhooks = ['https://discordapp.com/api/webhooks/716398429995270174/H3mAVClDg6gEnsjNipNe4hsePPwHx37UYIR9b5PFDFf8-jKrXkP8rOEQ5ipMjSRMmEdB', 'https://discordapp.com/api/webhooks/724822916331733032/PD1NKLSagHSMBs9ELwZV7u8cYFW8L9Gp3BWKHMiAGS2DRE2gWM7xWetiDxs-09n-BqIZ']
 myclient = pymongo.MongoClient("mongodb://daniel442li:KANQzahn5dF3VzZ@cluster0-shard-00-00-jq2na.mongodb.net:27017,cluster0-shard-00-01-jq2na.mongodb.net:27017,cluster0-shard-00-02-jq2na.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 print(myclient.list_database_names())
@@ -44,37 +42,40 @@ for products in flashSales:
     if collection.count_documents({ 'productID': productID }, limit = 1) != 0:
         pass
     else:
-        title = products['title']
-        url = "https://frenzy.sale/" + products['password']
-        image_url = (products['image_urls'][0])
-        price = str(products['price_range']['max'])
-        currency = str(products['shop']['currency'])
-        totPrice = price + ' ' + currency
-        start = (products['started_at'])
-        start = datetime.datetime.strptime(start,"%Y-%m-%dT%H:%M:%S.%fZ")
-        start = start - timedelta(hours = 4)
-        count = (products['products_count'])
-        shop = (products['shop']['name'])
-        shipping_message = (products['shipping_message'])
-        if start.minute == 0:
-            minute = '00'
-        else:
-            minute = start.minute
-        if start.second == 0:
-            second = '00'
-        else:
-            second = start.minute
-        start = f'{start.month}/{start.day}/{start.year}, {start.hour}:{minute}:{second} EST'
+        for WEBHOOK_URL in webhooks:
+            webhook = DiscordWebhooks(WEBHOOK_URL)
+            title = products['title']
+            url = "https://frenzy.sale/" + products['password']
+            image_url = (products['image_urls'][0])
+            price = str(products['price_range']['max'])
+            currency = str(products['shop']['currency'])
+            totPrice = price + ' ' + currency
+            start = (products['started_at'])
+            start = datetime.datetime.strptime(start,"%Y-%m-%dT%H:%M:%S.%fZ")
+            start = start - timedelta(hours = 4)
+            count = (products['products_count'])
+            shop = (products['shop']['name'])
+            shipping_message = (products['shipping_message'])
+            if start.minute == 0:
+                minute = '00'
+            else:
+                minute = start.minute
+            if start.second == 0:
+                second = '00'
+            else:
+                second = start.minute
+            start = f'{start.month}/{start.day}/{start.year}, {start.hour}:{minute}:{second} EST'
 
-        webhook.set_content(title=title, url=url)
-        webhook.set_thumbnail(url=image_url)
-        webhook.add_field(name="Price:", value=totPrice)
-        webhook.add_field(name="Store:", value=shop)
-        webhook.add_field(name="Item Count:", value=count)
-        webhook.add_field(name="Shipping", value = shipping_message)
-        webhook.add_field(name="Release Date:", value=start)
-        webhook.set_footer(icon_url = 'http://drive.google.com/uc?id=16ZsNOlvQmwI2rwjA0vnT7Z5mLiHGMqwk', text='Powered by Sudu Frenzy')
-        webhook.send()
+            webhook.set_content(title=title, url=url)
+            webhook.set_thumbnail(url=image_url)
+            webhook.add_field(name="Price:", value=totPrice)
+            webhook.add_field(name="Store:", value=shop)
+            webhook.add_field(name="Item Count:", value=count)
+            webhook.add_field(name="Shipping", value = shipping_message)
+            webhook.add_field(name="Release Date:", value=start)
+            webhook.set_footer(icon_url = 'http://drive.google.com/uc?id=16ZsNOlvQmwI2rwjA0vnT7Z5mLiHGMqwk', text='Powered by Sudu Frenzy')
+            webhook.send()
+
         mydict = {'productID': productID}
         x = collection.insert_one(mydict)
         print(f'Inserted: {productID}')
